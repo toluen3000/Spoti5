7c4658fd7788bae452264380459064e134540eaf
toluen3000 <toluen3000@gmail.com>
2025-07-19 20:42:15 +0700

    Repo Module

--- app/src/main/java/com/example/spoti5/di/RepositoryModule.kt
+++ app/src/main/java/com/example/spoti5/di/RepositoryModule.kt
@@ -2,9 +2,11 @@ package com.example.spoti5.di
 //
 
 import com.example.spoti5.data.repositories.AlbumsRepositoryImpl
+import com.example.spoti5.data.repositories.ArtistDetailRepositoryImpl
 import com.example.spoti5.data.repositories.SpotifyRepositoryImpl
 import com.example.spoti5.data.repositories.UserSavedAlbumRepositoryImpl
 import com.example.spoti5.domain.repository.AlbumsRepository
+import com.example.spoti5.domain.repository.ArtistDetailRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -35,4 +37,11 @@ abstract class RepositoryModule {
     abstract fun bindUserSavedAlbumRepository(
         impl: UserSavedAlbumRepositoryImpl
     ): UserSavedAlbumRepository
+
+
+    @Binds
+    @Singleton
+    abstract fun bindArtistDetailRepository(
+        impl: ArtistDetailRepositoryImpl
+    ): ArtistDetailRepository
 }
